98fcc668b0848e878a3f6607725483134e0e9a6d
João Felipe Lobato <89958345+Imperador-Joao@users.noreply.github.com>
2024-08-05 16:48:46 -0300

    Add files via upload

--- Massa Mola Amortecedor/Forçamento Harmônico Dist Normal/mma_normal_graficos_conv.m
+++ Massa Mola Amortecedor/Forçamento Harmônico Dist Normal/mma_normal_graficos_conv.m
@@ -4,8 +4,11 @@ figura = figure;
 figura.Position = [408,94,1118,857];
     
     subplot(2,2,1)
+    
+    hold on
+    yline(tolerancia,LineStyle="--")
     plot(vetor_qtd_amostras,abs(vetor_media_forcas-media_teorica),Color="b")
-
+    set(gca,'yscale','log')
 
     title("Convergência das Forças",FontSize=15)
     xlabel("Tamanho da Amostra",FontSize=15)
@@ -13,8 +16,11 @@ figura.Position = [408,94,1118,857];
     
     
     subplot(2,2,2)
-
+    
+    hold on
+    yline(tolerancia,LineStyle="--")
     plot(vetor_qtd_amostras,abs(vetor_media_frequencias-media_teorica/(2*pi)),Color="b")
+    set(gca,'yscale','log')
 
     title("Convergência das Frequências",FontSize=15)
     xlabel("Tamanho da Amostra",FontSize=15)
@@ -22,15 +28,21 @@ figura.Position = [408,94,1118,857];
 
     subplot(2,2,3)
     
+    hold on
+    yline(tolerancia,LineStyle="--")
     plot(vetor_qtd_amostras,abs(vetor_dp_forcas - desvio_padrao_teorico))
+    set(gca,'yscale','log')
 
     title("Convergência das Forças",FontSize=15)
     xlabel("Tamanho da Amostra",FontSize=15)
     ylabel(["Resíduos do Desvio Padrão";"das Amplitudes (N)"],FontSize=15)
 
     subplot(2,2,4)
-
+    
+    hold on
+    yline(tolerancia,LineStyle="--")
     plot(vetor_qtd_amostras,abs(vetor_dp_frequencias - desvio_padrao_teorico/(2*pi)))
+    set(gca,'yscale','log')
 
     title("Convergência das Frequências",FontSize=15)
     xlabel("Tamanho da Amostra",FontSize=15)
